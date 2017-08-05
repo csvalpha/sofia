6756e806c71197bf5a64448ee8f11dd6e0d74880
matthijs <vos1996@gmail.com>
2017-08-05 20:31:55 +0200

    Updated templates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,23 @@
-articles = []
-articles << FactoryGirl.create_list(:activity, 5)
+activities = []
+[0,1,2,3].each do
+  activities << FactoryGirl.create(:activity)
+end
+
+products = []
+[0,1,2,3].each do
+  products << FactoryGirl.create(:product)
+end
+
+products.each do |product|
+  activities.each do |activity|
+    # binding.pry
+    FactoryGirl.create(:product_price, product: product, price_list: activity.price_list)
+  end
+end
+
+activities.each do |activity|
+  # TODO remove amount, this should be calculated in model
+  [0,1,2].each do
+    FactoryGirl.create(:transaction, product: products.sample, activity: activity, amount: 8)
+  end
+end
