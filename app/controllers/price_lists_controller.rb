2b3a4a9a407fc09189a22d4fb9569df1895d9f0c
Casper Smits <casper@caspersmits.nl>
2017-09-29 00:01:36 +0200

    Add product price table to pricelists view

--- app/controllers/price_lists_controller.rb
+++ app/controllers/price_lists_controller.rb
@@ -1,7 +1,12 @@
 class PriceListsController < ApplicationController
   before_action :set_model, only: %i[show update destroy]
 
-  autocomplete :price_list, :name, { full: true }
+  autocomplete :price_list, :name, full: true
+
+  def index
+    @recent_price_lists = PriceList.order(:created_at).limit(6).includes(:product_price)
+    @products = Product.all
+  end
 
   def model_class
     PriceList
