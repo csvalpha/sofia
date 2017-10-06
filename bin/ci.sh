720dbd760e10478fb498c7488c553aa030808a10
Casper Smits <casper@caspersmits.nl>
2017-10-06 22:38:36 +0200

    Add slim-lint to CI

--- bin/ci.sh
+++ bin/ci.sh
@@ -4,6 +4,7 @@ set -e
 if [ "${TYPE}" = "lint" ] || [ "${TYPE}" = "" ]; then
   bundle exec rubocop
   bundle exec brakeman -z
+  bundle exec slim-lint
   gem install bundler-audit
   bundle-audit update && bundle-audit check
   RAILS_ENV=test bundle exec rails db:create db:environment:set db:schema:load
