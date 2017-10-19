0a0faa919b84aa08363a55c975b1382ca817caa9
Casper Smits <casper@caspersmits.nl>
2017-10-19 21:17:23 +0200

    Update activity form

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,6 @@
 class IndexController < ApplicationController
   def index
     @upcoming_activities = Activity.upcoming.limit(5)
-    @current_activity = Activity.current
+    @current_activities = Activity.current
   end
 end
