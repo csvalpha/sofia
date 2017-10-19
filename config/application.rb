723907d42dc36e6dc0e66bcc33b740a21ab356ed
Casper Smits <dev@caspersmits.nl>
2017-10-19 21:42:04 +0200

    Remove webpacker from repo (#14)

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Tomato
@@ -13,9 +11,5 @@ module Tomato
 
     config.i18n.default_locale = :nl
     config.i18n.available_locales = %i[en nl]
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
