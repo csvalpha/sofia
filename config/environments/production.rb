0327aadc241706b28cb39897dbb6c6877ee02880
Casper Smits <casper@caspersmits.nl>
2017-10-20 23:13:08 +0200

    fix oauth_url in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,8 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.action_mailer.default_url_options = { scheme: 'https', host: 'tomato.csvalpha.nl' }
+
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
