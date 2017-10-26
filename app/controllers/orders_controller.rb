af7a52c7dafe3f9ab816ce90c5ed79719ce10a45
Casper Smits <casper@caspersmits.nl>
2017-10-26 20:07:06 +0200

    Add initial orderscreen grid
    With the help of CSS Grid 🎉

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :set_model, only: %i[show update destroy]
+  before_action :set_model, only: %i[index new create]
   before_action :authenticate_user!
 
   def index
@@ -16,7 +16,11 @@ class OrdersController < ApplicationController
 
   private
 
+  def set_model
+    @activity = Activity.find_by(id: params[:activity_id])
+  end
+
   def permitted_attributes
-    params.require(:order).permit(%i[user order_row])
+    params.require(:order).permit(%i[user order_row activity_id])
   end
 end
