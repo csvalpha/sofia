c59fffdec6729e4e16b974e3dcd9570eeabd865f
Casper Smits <casper@caspersmits.nl>
2017-10-26 12:22:20 +0200

    Add icons to menus

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,6 @@
 class IndexController < ApplicationController
   def index
-    @upcoming_activities = Activity.upcoming.limit(5)
-    @current_activities = Activity.current
+    @upcoming_activities = Activity.upcoming.limit(5) || []
+    @current_activities = Activity.current || []
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def nav_link(name = nil, options = nil, html_options = nil, &block)
+    if current_page?(name)
+      options&.merge!(class: 'active') { |_, v1, v2| [v1, v2].join ' ' }
+    end
+
+    link_to(name, options, html_options) do
+      yield
+    end
+  end
 end
