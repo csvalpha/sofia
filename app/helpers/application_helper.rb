f56379ed891c064cb6414cf066cd7af430428737
Casper Smits <dev@caspersmits.nl>
2017-12-11 16:00:11 +0100

    Polishing (#35)
    * Add flash
    
    * Go to user when clicking on name
    
    * Update colspan on empty table message
    
    * Update home page with quote
    
    * Add flash
    
    * Align login prompt homepage
    
    * Polish flash warnings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
 
     link_to(name, options, html_options, &block)
   end
+
+  def flash_class(level)
+    case level
+    when 'notice' then 'alert-info'
+    when 'success' then 'alert-success'
+    when 'error' then 'alert-error'
+    when 'alert' then 'alert-error'
+    end
+  end
 end
