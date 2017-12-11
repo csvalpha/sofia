917316989055a30c5d5f91ac73e5b7e680edc9c6
Casper Smits <casper@caspersmits.nl>
2017-12-11 15:36:56 +0100

    Add flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
 
     link_to(name, options, html_options, &block)
   end
+
+  def flash_class(level)
+    case level
+    when :notice then 'alert alert-info'
+    when :success then 'alert alert-success'
+    when :error then 'alert alert-error'
+    when :alert then 'alert alert-error'
+    end
+  end
 end
