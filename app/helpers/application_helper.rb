ad33a6435edef3b96effe95750a70c441a92b63e
Casper Smits <casper@caspersmits.nl>
2017-12-11 15:59:10 +0100

    Polish flash warnings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,10 +9,10 @@ module ApplicationHelper
 
   def flash_class(level)
     case level
-    when :notice then 'alert alert-info'
-    when :success then 'alert alert-success'
-    when :error then 'alert alert-error'
-    when :alert then 'alert alert-error'
+    when 'notice' then 'alert-info'
+    when 'success' then 'alert-success'
+    when 'error' then 'alert-error'
+    when 'alert' then 'alert-error'
     end
   end
 end
