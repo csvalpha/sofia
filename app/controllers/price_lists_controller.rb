9e80d197e5144c0cc9e9bbffb1c71977cbfbb998
Casper Smits <casper@caspersmits.nl>
2017-12-15 13:21:52 +0100

    Streamline price_lists index and add new-modal

--- app/controllers/price_lists_controller.rb
+++ app/controllers/price_lists_controller.rb
@@ -6,10 +6,12 @@ class PriceListsController < ApplicationController
   autocomplete :price_list, :name, full: true
 
   def index
-    recent_price_lists = PriceList.limit(6)
+    recent_price_lists = PriceList.order(created_at: :desc).limit(5)
     products = Product.all.order(:id).includes(:product_prices)
 
     authorize recent_price_lists
+
+    @new_price_list = PriceList.new
 
     @recent_price_lists_json = recent_price_lists.to_json(except: %i[created_at updated_at deleted_at])
     @products_json = products.to_json(include: { product_prices: { except: %i[created_at updated_at deleted_at] } },
@@ -21,7 +23,20 @@ class PriceListsController < ApplicationController
     authorize @model
   end
 
+  def create
+    @price_list = PriceList.new(permitted_attributes)
+    authorize @price_list
+
+    if @price_list.save
+      flash[:success] = 'Prijslijst aangemaakt'
+    else
+      flash[:error] = "Prijslijst aanmaken mislukt; #{@price_list.errors.full_messages.join(', ')}"
+    end
+    redirect_to price_lists_path
+  end
+
   def update
+    authorize @model
     render json: @model if @model.update(permitted_attributes)
   end
 
