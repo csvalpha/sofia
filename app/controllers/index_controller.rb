59dc20804b2405b16f3a21f19a999f5046398b9b
matthijs <vos1996@gmail.com>
2017-12-23 21:19:24 +0100

    Rubocop

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,7 +2,7 @@ class IndexController < ApplicationController
   after_action :verify_authorized, except: :index
 
   def index
-    redirect_to current_user if current_user && current_user.roles_users.empty?
+    redirect_to current_user if current_user&.roles_users&.empty?
     @upcoming_activities = Activity.upcoming.limit(5)
     @current_activities = Activity.current
   end
