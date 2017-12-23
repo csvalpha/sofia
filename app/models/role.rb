96da0b90bded35b2cf19df30e85adf50b7814a99
matthijs <vos1996@gmail.com>
2017-12-23 21:10:57 +0100

    Redirect when no role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,6 @@
 class Role < ApplicationRecord
   validates :name, inclusion: { in: ['Treasurer', 'Main Bartender'] }
   validates :group_uid, presence: true
+
+  has_many :role_users, dependent: :destroy
 end
