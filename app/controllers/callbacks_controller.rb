25be0474eac01c64020ed6e926f4c61e526bddb7
matthijs <vos1996@gmail.com>
2017-12-24 15:08:30 +0100

    Rubocop

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,10 +1,10 @@
 class CallbacksController < Devise::OmniauthCallbacksController
-  def banana_oauth2
+  def banana_oauth2 # rubocop:disable Metrics/AbcSize
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
       sign_in(:user, @user)
-      redirect_to root_path, flash: { success: 'Authenticated with C.S.V. Alpha' }  if @user&.roles&.any?
+      redirect_to root_path, flash: { success: 'Authenticated with C.S.V. Alpha' } if @user&.roles&.any?
       redirect_to @user, flash: { success: 'Authenticated with C.S.V. Alpha' } unless @user&.roles&.any?
     else
       redirect_to root_path, flash: { error: 'Authentication failed' }
