ef37dcc3eea70abce3311209e79f7179d44fdee6
matthijs <vos1996@gmail.com>
2017-12-24 15:19:00 +0100

    Do a protected redirect to user

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -5,9 +5,16 @@ class CallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       sign_in(:user, @user)
       redirect_to root_path, flash: { success: 'Authenticated with C.S.V. Alpha' } if @user&.roles&.any?
-      redirect_to @user, flash: { success: 'Authenticated with C.S.V. Alpha' } unless @user&.roles&.any?
+      redirect_to_user(@user) unless @user&.roles&.any?
     else
       redirect_to root_path, flash: { error: 'Authentication failed' }
     end
+  end
+
+  private
+
+  def redirect_to_user(user)
+    new_user = User.find(user.id)
+    redirect_to new_user, flash: { success: 'Authenticated with C.S.V. Alpha' }
   end
 end
