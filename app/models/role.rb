631a4b3ad8992c0a9192515426ddcb8cfcc96d20
matthijs <vos1996@gmail.com>
2017-12-24 08:40:10 +0100

    Fix relationships

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,5 +2,6 @@ class Role < ApplicationRecord
   validates :name, inclusion: { in: ['Treasurer', 'Main Bartender'] }
   validates :group_uid, presence: true
 
-  has_many :role_users, dependent: :destroy
+  has_many :roles_users, class_name: RolesUsers, dependent: :destroy
+  has_many :users, through: :roles_users
 end
