e7f69c702689bef67d0d70d8be117b856154991a
Matthijsy <vos1996@gmail.com>
2017-12-27 18:01:20 +0100

    Feature/no roles (#50)
    * Redirect when no role
    
    * Update policy
    
    * Rubocop
    
    * Fix relationships
    
    * Fix redirect
    
    * Rubocop
    
    * Fix security issue
    
    * Rubocop
    
    * Do a protected redirect to user
    
    * Less AbcSize
    
    * Allow treasurer to access user#show
    
    * Better code
    
    * redirect
    
    * Hopefully safe now

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,10 +1,10 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def banana_oauth2
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+    user = User.from_omniauth(request.env['omniauth.auth'])
 
-    if @user.persisted?
-      sign_in(:user, @user)
-      redirect_to root_path, flash: { success: 'Authenticated with C.S.V. Alpha' }
+    if user.persisted?
+      sign_in(:user, user)
+      redirect_to user.roles.any? ? root_path : user_path(user.id)
     else
       redirect_to root_path, flash: { error: 'Authentication failed' }
     end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,4 +2,8 @@ class UserPolicy < ApplicationPolicy
   def refresh_user_list?
     user&.treasurer?
   end
+
+  def show?
+    user&.treasurer? || record == user
+  end
 end
