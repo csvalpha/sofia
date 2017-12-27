cdba3492fdf8ebe04941f7a5b66a4e9a25e4a0af
matthijs <vos1996@gmail.com>
2017-12-27 13:20:06 +0100

    Remove show and update

--- app/controllers/product_price_controller.rb
+++ app/controllers/product_price_controller.rb
@@ -1,5 +1,5 @@
 class ProductPriceController < ApplicationController
-  before_action :set_model, only: %i[show update destroy]
+  before_action :set_model, only: %i[destroy]
   before_action :authenticate_user!
 
   after_action :verify_authorized
