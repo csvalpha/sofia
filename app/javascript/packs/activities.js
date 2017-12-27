c115113fb8bf3d8051459f3c496aa3009d38833c
matthijs <vos1996@gmail.com>
2017-12-27 13:13:05 +0100

    Kebab_case

--- app/javascript/packs/activities.js
+++ app/javascript/packs/activities.js
@@ -8,7 +8,7 @@ Vue.use(VueResource);
 document.addEventListener('turbolinks:load', () => {
   Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
-  var element = document.getElementById('editActivityModal');
+  var element = document.getElementById('edit_activity_modal');
   if (element !== null) {
     var price_lists = JSON.parse(element.dataset.priceLists);
     var vueActivities = new Vue({
