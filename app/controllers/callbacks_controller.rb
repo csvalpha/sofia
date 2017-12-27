12c0643beaf2436f148cfcd0dd74a0446e5b3790
matthijs <vos1996@gmail.com>
2017-12-27 13:18:37 +0100

    Better code

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,20 +1,12 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def banana_oauth2
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+    user = User.from_omniauth(request.env['omniauth.auth'])
 
-    if @user.persisted?
-      sign_in(:user, @user)
-      success_redirect(@user)
+    if user.persisted?
+      sign_in(:user, user)
+      redirect_to user.roles.any? ? root_path : User.find(user.id)
     else
       redirect_to root_path, flash: { error: 'Authentication failed' }
     end
-  end
-
-  private
-
-  def success_redirect(user)
-    flash[:success] = 'Authenticated with C.S.V. Alpha'
-    redirect_to root_path if user.roles.any?
-    redirect_to User.find(user.id) unless user.roles.any?
   end
 end
