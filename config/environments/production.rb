89afecf78dac595bc94face308fd0387f3e44707
Casper Smits <casper@caspersmits.nl>
2017-12-29 18:58:37 +0100

    Enable bullet notifications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.sentry = true
+  end
 end
