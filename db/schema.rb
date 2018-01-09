55395c71089286300fa85dcc6445d1910af167a9
Matthijsy <vos1996@gmail.com>
2018-01-09 21:22:05 +0100

    Add avatar from banana (#98)
    * Add avatar from banana
    
    * Also show avatar when selected
    
    * Add fallback when image doesn't exist
    
    * Find_or_create -> find_or_initialize
    
    * Do not use uid, use full url
    
    * Add fallback
    
    * Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180107182815) do
+ActiveRecord::Schema.define(version: 20180107221009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 20180107182815) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.string "avatar_thumb_url"
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
