117526989582dad5091d3d4cb9bae260645e696a
matthijs <vos1996@gmail.com>
2018-03-05 19:16:57 +0100

    Fix spec

--- spec/jobs/negative_credit_mailer_job_spec.rb
+++ spec/jobs/negative_credit_mailer_job_spec.rb
@@ -4,20 +4,22 @@ RSpec.describe NegativeCreditMailerJob, type: :job do
   describe '#perform' do
     let(:user) { FactoryBot.create(:user) }
     let(:negative_user) { FactoryBot.create(:user) }
-    let(:mail_unknown_user) { FactoryBot.create(:user, email: nil) }
+    let(:mail_unknown_negative_user) { FactoryBot.create(:user, email: nil, provider: 'some_external_source') }
+    let(:mail_unknown_user) { FactoryBot.create(:user, email: nil, provider: 'some_external_source') }
     let(:treasurer) { FactoryBot.create(:user, roles: [FactoryBot.create(:role, role_type: :treasurer)]) }
     let(:mail) { ActionMailer::Base.deliveries }
 
     subject(:job) do
-      # perform_enqueued_jobs do
-      NegativeCreditMailerJob.perform_now
-      # end
+      perform_enqueued_jobs do
+        NegativeCreditMailerJob.perform_now
+      end
     end
 
     before do
       ActionMailer::Base.deliveries = []
       user
       FactoryBot.create(:credit_mutation, user: negative_user, amount: -2)
+      FactoryBot.create(:credit_mutation, user: mail_unknown_negative_user, amount: -2)
       mail_unknown_user
       treasurer
       job
@@ -27,6 +29,7 @@ RSpec.describe NegativeCreditMailerJob, type: :job do
     it { expect(mail.first.to.first).to eq negative_user.email }
     it { expect(mail.first.body.to_s).to include '-2' }
     it { expect(mail.second.to.first).to eq treasurer.email }
-    it { expect(mail.second.body.to_s).to include mail_unknown_user.name }
+    it { expect(mail.second.body.to_s).to include mail_unknown_negative_user.name }
+    it { expect(mail.second.body.to_s).not_to include mail_unknown_user.name }
   end
 end
