e74d034d4855a92549b02568b3cf835f15256af9
Twan Coenraad <info@twancoenraad.nl>
2018-03-26 21:08:40 +0200

    Add missing credit mutations total

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -33,15 +33,19 @@ class ActivitiesController < ApplicationController
                                   credit_mutations: [:user]).find(params[:id])
     authorize @activity
 
+    @price_list = @activity.price_list
     @bartenders = @activity.bartenders
     @orders = @activity.orders
+
     @credit_mutations = @activity.credit_mutations
-    @price_list = @activity.price_list
+    @credit_mutations_total = @activity.credit_mutations_total
+
     @revenue_by_category = @activity.revenue_by_category
     @revenue_with_cash = @activity.revenue_with_cash
     @revenue_without_cash = @activity.revenue_without_cash
     @cash_total = @activity.revenue_with_cash + @activity.credit_mutations_total
     @revenue_total = @activity.revenue_with_cash + @activity.revenue_without_cash
+
     @count_per_product = @activity.count_per_product
   end
 
