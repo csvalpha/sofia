2681907cef7082fb59fbbefc6018a4c960ec494b
Matthijsy <vos1996@gmail.com>
2018-03-31 20:19:30 +0200

    Add minority warnings (#135)
    * Read email and birthday field from banan
    
    * Add warnings when ordering on minor user
    
    * Add 18+ in order list
    
    * Add minor warnings to order screen
    
    * Add specs
    
    * Update schema.rb
    
    * Xo
    
    * Re-add requires_age to product
    
    * Re-add requires_age to product
    
    * Update layout
    
    * Fix migrations
    
    * determine requires_age based on category
    
    * Remove requires age from price_list#index
    
    * Rmove ? from requires_age?
    
    * Rename minor_age to minor
    
    * Improve style
    
    * Improve scss name and made less grey
    
    * Remove email from user.show
    
    * Fix spec name
    
    * Fix schema.rb
    
    * Fix merge conflicts
    
    * Position 18+ icon on the right top
    
    * Fix spec
    
    * Rubocop
    
    * Make 18+ icon more gray
    
    * Fix stylesheet order

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -55,10 +55,12 @@ class ActivitiesController < ApplicationController
     @activity = Activity.includes([:price_list, price_list: { product_price: :product }])
                         .find(params[:id])
 
-    @product_prices_json = sorted_product_price(@activity).to_json(include: { product: { only: %i[id name category] } })
+    @product_prices_json = sorted_product_price(@activity).to_json(
+      include: { product: { only: %i[id name category], methods: %i[requires_age] } }
+    )
 
     @users_json = User.includes(%i[credit_mutations order_rows]).order(:name)
-                      .to_json(only: %i[id name], methods: %i[credit avatar_thumb_or_default_url])
+                      .to_json(only: %i[id name], methods: %i[credit minor avatar_thumb_or_default_url])
     @activity_json = @activity.to_json(only: %i[id title start_time end_time])
 
     render layout: 'order_screen'
