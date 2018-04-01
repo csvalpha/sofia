71a393d03b849f6ee7c94929eceb5e6d1b03cec7
Casper Smits <casper@caspersmits.nl>
2018-04-01 21:07:06 +0200

    Add placeholder buttons and improve styling

--- app/javascript/orderscreen/orderhistory.vue
+++ app/javascript/orderscreen/orderhistory.vue
@@ -1,27 +1,37 @@
 <template lang="html">
   <b-row class="order-history">
-    <b-table small hover :busy.sync="isLoading" :items="ordersProvider" :fields="fields"
-      no-provider-sorting sort-by="created_at" sort-desc>
-      <template slot="show_details" slot-scope="row">
-       <i @click.stop="row.toggleDetails" :class="['fa', 'fa-lg', row.detailsShowing ? 'fa-chevron-circle-up' : 'fa-chevron-circle-down']"></i>
-      </template>
-      <div slot="row-details" slot-scope="row">
-        <b-row v-for="row in row.item.order_rows">
-          <b-col></b-col>
-          <b-col>{{row.product.name}}</b-col>
-          <b-col>
-            {{row.product_count}}
-            {{' x '}}
-            {{doubleToCurrency(row.price_per_product)}}
-          </b-col>
-          <b-col>
-            {{doubleToCurrency(row.product_count * row.price_per_product)}}
-          </b-col>
-        </b-row>
-      </div>
-    </b-table>
+    <b-col>
+      <b-table small hover :busy.sync="isLoading" :items="ordersProvider" :fields="fields"
+        no-provider-sorting sort-by="created_at" sort-desc>
+        <template slot="order_total" slot-scope="row">
+          <span class="pull-right">
+            {{doubleToCurrency(row.item.order_total)}}
+            <i @click.stop="row.toggleDetails" :class="['order-history--row-edit', 'fa', 'fa-lg', 'pl-2', row.detailsShowing ? 'fa-chevron-circle-up' : 'fa-chevron-circle-down']"></i>
+          </span>
+        </template>
+        <div slot="row-details" slot-scope="row">
+          <b-container>
+            <b-row v-for="row in row.item.order_rows">
+              <b-col sm="2" class="text-right">-</b-col>
+              <b-col sm="4">{{row.product.name}}</b-col>
+              <b-col>
+                {{row.product_count}}
+                {{' x '}}
+                {{doubleToCurrency(row.price_per_product)}}
+              </b-col>
+              <b-col sm="2" class="pr-0">
+                <span class="pull-right">
+                  {{doubleToCurrency(row.product_count * row.price_per_product)}}
+                  <i class="order-history--row-edit fa fa-lg fa-edit pl-1"></i>
+                </span>
+              </b-col>
+            </b-row>
+          </b-container>
+        </div>
+      </b-table>
 
-    <spinner class="pt-2 pb-3 m-auto" size="large" v-if="isLoading" />
+      <spinner class="pt-2 pb-3 m-auto" size="large" v-if="isLoading" />
+    </b-col>
   </b-row>
 </template>
 
@@ -44,7 +54,9 @@ export default {
       fields: {
         id: {
           label: '#',
-          sortable: true
+          sortable: true,
+          thClass: 'text-center',
+          tdClass: 'text-center'
         },
         created_at: {
           label: 'Tijdstip',
@@ -59,11 +71,7 @@ export default {
         order_total: {
           label: 'Bedrag',
           sortable: false,
-          formatter: 'doubleToCurrency'
-        },
-        show_details: {
-          label: ' ',
-          sortable: false
+          thClass: 'text-right pr-4'
         }
       },
     };
