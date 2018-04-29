9b2d6c31cf0c9a6650ee16325e285b1222cbefe9
Casper Smits <casper@caspersmits.nl>
2018-04-29 12:12:37 +0200

    Use html type for mails

--- app/mailers/user_credit_mailer.rb
+++ app/mailers/user_credit_mailer.rb
@@ -1,13 +1,13 @@
 class UserCreditMailer < ApplicationMailer
   def insufficient_credit_mail(user)
     @user = user
-    mail to: user.email, subject: 'Verzoek met betrekking op uw Zatladder saldo', content_type: 'text/plain'
+    mail to: user.email, subject: 'Verzoek met betrekking op uw Zatladder saldo'
   end
 
   def credit_delivery_report_mail(treasurer, success_count, unnotifyable_users)
     @user = treasurer
     @unnotifyable_users = unnotifyable_users
     @success_count = success_count
-    mail to: treasurer.email, subject: 'Er is een saldomail verstuurd', content_type: 'text/plain'
+    mail to: treasurer.email, subject: 'Er is een saldomail verstuurd'
   end
 end
