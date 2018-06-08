0c4ef970345b6c3b0788d11b687891a70e9bf438
Casper Smits <casper@caspersmits.nl>
2018-06-08 17:35:31 +0200

    Convert users table to Vue table with sorting

---
+++ app/javascript/packs/users.js
@@ -0,0 +1,43 @@
+import Vue from 'vue/dist/vue.esm';
+import TurbolinksAdapter from 'vue-turbolinks';
+import axios from 'axios';
+import BootstrapVue from 'bootstrap-vue';
+
+Vue.use(TurbolinksAdapter);
+Vue.use(BootstrapVue);
+
+document.addEventListener('turbolinks:load', () => {
+  axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+  var element = document.getElementById('users-index');
+  if (element !== null) {
+    var users = JSON.parse(element.dataset.users);
+    new Vue({
+      el: element,
+      data: () => {
+        return {
+          users: users,
+          fields: {
+            id: {
+              label: 'ID',
+              sortable: true,
+              isRowHeader: true
+            },
+            name: {
+              label: 'Naam',
+              sortable: true
+            },
+            credit: {
+              label: 'Saldo',
+              sortable: true,
+              tdClass: (value) => {
+                return value <= 0 ? 'text-danger' : '';
+              },
+              formatter: (value) => `€${parseFloat(value).toFixed(2)}`,
+            }
+          }
+        };
+      },
+    });
+  }
+});
