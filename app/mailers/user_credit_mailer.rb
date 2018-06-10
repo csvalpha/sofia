92003d1a88d383c8e5c20b9e6e7e6d08a67cdfe1
Casper Smits <casper@caspersmits.nl>
2018-06-10 23:58:44 +0200

    Make text a little better readible

--- app/mailers/user_credit_mailer.rb
+++ app/mailers/user_credit_mailer.rb
@@ -19,11 +19,11 @@ class UserCreditMailer < ApplicationMailer
 
   def new_credit_mutation_mail(credit_mutation)
     @user = credit_mutation.user
-    @title = 'Bedankt voor het inleggen'
+    @title = credit_mutation.amount.positive? ? 'Bedankt voor het inleggen' : 'Je saldo is bijgewerkt'
     @credit_mutation_amount = credit_mutation.amount
     @credit_mutation_description = credit_mutation.description
 
-    subject = 'Je betaling aan flux is verwerkt'
+    subject = credit_mutation.amount.positive? ? 'Je betaling aan flux is verwerkt' : 'Je saldo is bijgewerkt'
 
     mail to: credit_mutation.user.email, subject: subject
   end
