79348867b8f2b1a2403649128a5900432c2e31e6
Casper Smits <casper@caspersmits.nl>
2018-06-23 21:14:11 +0200

    Move mail notification to job and add specs

--- app/controllers/credit_mutations_controller.rb
+++ app/controllers/credit_mutations_controller.rb
@@ -15,7 +15,7 @@ class CreditMutationsController < ApplicationController
 
     respond_to do |format|
       if @mutation.save
-        UserCreditMailer.new_credit_mutation_mail(@mutation).deliver_now
+        NewCreditMutationNotificationJob.perform_later(@mutation)
         format.html { redirect_to request.referer, success: 'Successfully created mutation' }
         format.json { render json: @mutation, include: { user: { methods: %i[credit avatar_thumb_or_default_url] } } }
 
