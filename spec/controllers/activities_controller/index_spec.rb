958a511598fb93a43ef2b31776736ce103ac5946
matthijs <vos1996@gmail.com>
2019-12-06 14:07:17 +0100

    Add spec

--- spec/controllers/activities_controller/index_spec.rb
+++ spec/controllers/activities_controller/index_spec.rb
@@ -2,20 +2,45 @@ require 'rails_helper'
 
 describe ActivitiesController, type: :controller do
   describe 'GET index' do
-    let(:activities) { FactoryBot.create_list(:activity, 3) }
+    let(:unlocked_activities) { FactoryBot.create(:activity) }
+    let(:locked_activities) { FactoryBot.create_list(:activity, 2, :locked) }
     let(:price_lists) { FactoryBot.create_list(:price_list, 3) }
+    let(:request) { get :index }
 
     before do
-      activities
+      unlocked_activities
+      locked_activities
       price_lists
+      sign_in user
+      request
     end
 
-    it 'assigns the values' do
-      sign_in FactoryBot.create(:user, :treasurer)
-      get :index
-      expect(assigns(:activities).size).to eq activities.size
-      expect(assigns(:price_lists_json)).to eq PriceList.all.to_json(only: %i[id name])
-      expect(assigns(:activity)).to be_an_instance_of(Activity)
+    describe 'when without permissions' do
+      let(:user) { FactoryBot.create(:user) }
+
+      it { expect(request.status).to eq 403 }
+    end
+
+    describe 'when as main-bartender' do
+      let(:user) { FactoryBot.create(:user, :main_bartender) }
+
+      it do
+        expect(request.status).to eq 200
+        expect(assigns(:activities).size).to eq 1
+        expect(assigns(:price_lists_json)).to eq PriceList.all.to_json(only: %i[id name])
+        expect(assigns(:activity)).to be_an_instance_of(Activity)
+      end
+    end
+
+    describe 'when as treasurer' do
+      let(:user) { FactoryBot.create(:user, :treasurer) }
+
+      it do
+        expect(request.status).to eq 200
+        expect(assigns(:activities).size).to eq 3
+        expect(assigns(:price_lists_json)).to eq PriceList.all.to_json(only: %i[id name])
+        expect(assigns(:activity)).to be_an_instance_of(Activity)
+      end
     end
   end
 end
