4a97ed7134196760ea53418d3e97f6689ddc5a89
matthijs <vos1996@gmail.com>
2020-03-08 15:41:01 +0100

    Implement invoice routes

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -6,5 +6,25 @@ class InvoicesController < ApplicationController
     authorize Invoice
 
     @invoices = Invoice.all
+    @activities_json = Activity.all.to_json(only: %i[id title])
+    @invoice = Invoice.new
+
+  end
+
+  def create
+    @invoice = Invoice.new(permitted_attributes)
+    authorize @invoice
+
+    if @invoice.save
+      flash[:success] = 'Successfully created invoice'
+    else
+      flash[:error] = @invoice.errors.full_messages.join(', ')
+    end
+  end
+
+  private
+
+  def permitted_attributes
+    params.require(:invoice).permit(%i[user activity])
   end
 end
