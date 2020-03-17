8319b8a5605409aadfe94a37c6c1337dca97e12d
matthijs <vos1996@gmail.com>
2020-03-17 13:28:37 +0100

    Always ensure pidfile is present

--- config/puma.rb
+++ config/puma.rb
@@ -19,6 +19,9 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
 
+# Ensure that the pidfile path exists
+Pathname(pidfile).dirname.mkpath
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
