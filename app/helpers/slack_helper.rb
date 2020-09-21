41af3cf13f5da4a52623ace822f7151a73bf79c6
matthijs <vos1996@gmail.com>
2020-09-21 16:48:24 +0200

    Fix rubocop

--- app/helpers/slack_helper.rb
+++ app/helpers/slack_helper.rb
@@ -4,12 +4,10 @@ module SlackHelper
   private
 
   def notifier
-    # rubocop:disable Rails/HelperInstanceVariable
     @notifier ||= Slack::Notifier.new(
       Rails.application.config.x.slack_webhook,
       username: 'SOFIA',
       channel: Rails.application.config.x.slack_channel
     )
-    # rubocop:enable Rails/HelperInstanceVariable
   end
 end
