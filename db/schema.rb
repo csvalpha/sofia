efe8604c570eafb3d66429cc8c8dfcb929e69062
matthijs <vos1996@gmail.com>
2020-10-07 14:38:03 +0200

    Add override for email and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_160044) do
+ActiveRecord::Schema.define(version: 2020_10_07_121611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2020_09_28_160044) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email_override"
+    t.string "name_override"
     t.index ["activity_id"], name: "index_invoices_on_activity_id"
     t.index ["user_id"], name: "index_invoices_on_user_id"
   end
