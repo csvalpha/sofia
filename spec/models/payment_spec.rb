a664fa4d8431306b776e56f35618181ee24e1831
matthijs <vos1996@gmail.com>
2020-11-20 22:36:58 +0100

    Increase coverage

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -6,8 +6,14 @@ RSpec.describe Payment, type: :model do
   describe '#valid' do
     it { expect(payment).to be_valid }
 
-    context 'when without a user' do
-      subject(:payment) { FactoryBot.build_stubbed(:payment, user: nil) }
+    context 'when without user and invoice' do
+      subject(:payment) { FactoryBot.build_stubbed(:payment, user: nil, invoice: nil) }
+
+      it { expect(payment).not_to be_valid }
+    end
+
+    context 'when with user and invoice' do
+      subject(:payment) { FactoryBot.build_stubbed(:payment, invoice: FactoryBot.create(:invoice)) }
 
       it { expect(payment).not_to be_valid }
     end
