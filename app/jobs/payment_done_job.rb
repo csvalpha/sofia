9a922558a265f8ed86e739f69161b1e342db6bd1
matthijs <vos1996@gmail.com>
2020-11-20 23:02:02 +0100

    Add handling the callbacks

---
+++ app/jobs/payment_done_job.rb
@@ -0,0 +1,33 @@
+class PaymentDoneJob < ApplicationJob
+  queue_as :default
+
+  def perform(payment)
+    if payment.user
+      process_user(payment)
+    end
+    if payment.invoice
+      process_invoice(payment)
+    end
+
+    payment.invoice.update(status: 'paid')
+  end
+
+  private
+
+  def process_user(payment)
+    mutation = CreditMutation.create(user: payment.user,
+                                     amount: payment.mollie_payment.amount.value,
+                                     description: 'iDEAL inleg', created_by: payment.user)
+
+    UserCreditMailer.new_credit_mutation_mail(mutation).deliver_later
+  end
+
+  def process_invoice(payment)
+    CreditMutation.create(user: payment.invoice.user,
+                                     amount: payment.mollie_payment.amount.value,
+                                     description: "Betaling factuur #{payment.invoice.human_id}", created_by: payment.user)
+
+    InvoiceMailer.invoice_paid(payment.invoice).deliver_now
+
+  end
+end
