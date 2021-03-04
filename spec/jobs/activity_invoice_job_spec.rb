5819bffa990a41f85e99acee076eab5d87261a36
matthijs <vos1996@gmail.com>
2021-03-04 14:21:51 +0100

    Fix specs

--- spec/jobs/activity_invoice_job_spec.rb
+++ spec/jobs/activity_invoice_job_spec.rb
@@ -5,13 +5,10 @@ RSpec.describe ActivityInvoiceJob, type: :job do
     let(:activity) { FactoryBot.create(:activity) }
     let(:manual_users) { FactoryBot.create_list(:user, 3) }
     let(:external_users) { FactoryBot.create_list(:user, 2, provider: 'some_provider') }
-    let(:emails) { ActionMailer::Base.deliveries }
 
     subject(:job) { perform_enqueued_jobs { described_class.perform_now(activity) } }
 
     before do
-      ActionMailer::Base.deliveries = []
-
       manual_users.each { |u| FactoryBot.create(:order_with_items, user: u, activity: activity) }
       external_users.each { |u| FactoryBot.create(:order_with_items, user: u, activity: activity) }
       activity.update(locked_by: FactoryBot.create(:user))
@@ -20,9 +17,8 @@ RSpec.describe ActivityInvoiceJob, type: :job do
     end
 
     context 'when with manual user orders' do
-      it { expect(emails.size).to eq 3 }
       it { expect(Invoice.count).to eq 3 }
-      it { expect(Invoice.last.status).to eq 'sent' }
+      it { expect(Invoice.last.status).to eq 'pending' }
     end
   end
 end
