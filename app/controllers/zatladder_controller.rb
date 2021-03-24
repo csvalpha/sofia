ff61bd34526d41efb386920ec6388f30f846acca
Matthijs Vos <vos1996@gmail.com>
2021-03-24 21:48:08 +0100

    Make it possible to select zatladder year (#585)

--- app/controllers/zatladder_controller.rb
+++ app/controllers/zatladder_controller.rb
@@ -1,18 +1,27 @@
 class ZatladderController < ApplicationController
-  include OctoberHelper
-
   before_action :authenticate_user!
   after_action :verify_authorized
 
   def index
     authorize :zatladder
 
-    @from_date  = last_october
-    @to_date    = last_october + 1.year
+    @year_range = 2018..current_year
+    @year = params[:year] || current_year
+    @from_date  = Time.zone.local(@year.to_i - 1, 10, 1)
+    @to_date    = @from_date + 1.year
+
     @zatladder = zatladder_spendings(@from_date, @to_date)
   end
 
   private
+
+  def current_year
+    if Time.zone.now.month < 10
+      Time.zone.now.year
+    else
+      Time.zone.now.year + 1
+    end
+  end
 
   def zatladder_spendings(from, to)
     @users_spendings = User.in_banana.calculate_spendings(from: from, to: to)
