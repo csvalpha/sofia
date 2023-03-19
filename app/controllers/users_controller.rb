a01992d2145b6027c0934150b54e34ba14f3235f
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2023-03-19 13:14:33 +0100

    Fix user order history sorting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -107,7 +107,7 @@ class UsersController < ApplicationController # rubocop:disable Metrics/ClassLen
     authorize user
 
     user_orders = policy_scope(Order).orders_for(user)
-    activities = Activity.select(%i[id title start_time]).joins(:orders).merge(user_orders).distinct
+    activities = Activity.select(%i[id title start_time]).joins(:orders).merge(user_orders).distinct.order(start_time: :desc, id: :desc)
     activity_totals = user_orders.joins(:order_rows).group(:activity_id).sum('product_count * price_per_product')
     activities_hash = activities.map { |a| { id: a.id, title: a.title, start_time: a.start_time, order_total: activity_totals[a.id] } }
 
