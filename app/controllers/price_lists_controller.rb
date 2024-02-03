9428f9f60b7d90786b1699b5bbdf8339a5173d27
Ellen Wittingen <ellen2000w@gmail.com>
2024-02-03 15:26:29 +0100

    Made price-lists table scrollable and made lists archivable

--- app/controllers/price_lists_controller.rb
+++ app/controllers/price_lists_controller.rb
@@ -4,14 +4,14 @@ class PriceListsController < ApplicationController
   after_action :verify_authorized
 
   def index
-    recent_price_lists = PriceList.order(created_at: :desc).limit(6)
+    price_lists = PriceList.order(created_at: :desc)
     products = Product.all.order(:id).includes(:product_prices)
 
-    authorize recent_price_lists
+    authorize price_lists
 
     @price_list = PriceList.new
 
-    @recent_price_lists_json = recent_price_lists.to_json(except: %i[created_at updated_at deleted_at])
+    @price_lists_json = price_lists.to_json(except: %i[created_at updated_at deleted_at])
     @products_json = products.to_json(
       include: { product_prices: { except: %i[created_at updated_at deleted_at] } },
       methods: :t_category,
@@ -41,6 +41,36 @@ class PriceListsController < ApplicationController
       flash[:error] = "Prijslijst wijzigen mislukt; #{@price_list.errors.full_messages.join(', ')}"
     end
     redirect_to @price_list
+  end
+
+  def archive
+    @price_list = PriceList.find(params[:id])
+    authorize @price_list
+
+    @price_list.archived_at = Time.zone.now
+
+    respond_to do |format|
+      if @price_list.save
+        format.json { render json: @price_list.archived_at }
+      else
+        format.json { render json: @price_list.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def unarchive
+    @price_list = PriceList.find(params[:id])
+    authorize @price_list
+
+    @price_list.archived_at = nil
+    
+    respond_to do |format|
+      if @price_list.save
+        format.json { render json: @price_list.archived_at }
+      else
+        format.json { render json: @price_list.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
