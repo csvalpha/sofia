291ce787b0028d539155b1cdeaf2e5a98f25b30b
Jorai Geertsema <jamestreeg@gmail.com>
2025-01-06 15:23:25 +0100

    Revert "update all bundler dependencies (#745)"
    This reverts commit 0a7b4be3833e86b90151a67026b625e0ab8153b7.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  include Pundit::Authorization
+  include Pundit
 
   before_action :set_sentry_context
   before_action :set_paper_trail_whodunnit
