8bfdf35a98c3b1881b76268479e1c7bb37899adc
Jorai Geertsema <jamestreeg@gmail.com>
2025-01-08 18:43:57 +0100

    updated gemfile

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(*Rails.groups)
 module Sofia
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     config.time_zone = 'Europe/Amsterdam'
 
