d1212d6eb08506629e2dcbfecd67cb3a346cd74b
Lodewiges <jamestreeg@gmail.com>
2025-09-15 20:27:27 +0200

    lint fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -23,12 +23,12 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
+  if Rails.root.join('tmp', 'caching-dev.txt', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
-    config.public_file_server.headers = { "Cache-Control" => "public, max-age=#{2.days.to_i}" }
+    config.public_file_server.headers = { 'Cache-Control' => "public, max-age=#{2.days.to_i}" }
   else
     config.action_controller.perform_caching = false
 
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Set the default host and port for links in emails to the main Rails app.
-  config.action_mailer.default_url_options = { host: "localhost", port: 5000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
 
   # Set the asset host for images/styles in emails to the Webpacker dev server.
   config.action_mailer.asset_host = 'http://localhost:5000'
