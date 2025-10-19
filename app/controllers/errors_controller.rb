439db22f08ff2b46d4e2a013bf1b6a0a048fdf0b
Lodewiges <jamestreeg@gmail.com>
2025-10-19 23:10:50 +0200

    improved error handeling

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,17 +1,18 @@
 class ErrorsController < ApplicationController
+
   def forbidden
-    render status: :forbidden
+    render file: Rails.root.join('public', '403.html'), layout: false, status: :forbidden
   end
 
   def not_found
-    render status: :not_found
+    render file: Rails.root.join('public', '404.html'), layout: false, status: :not_found
   end
 
   def unacceptable
-    render status: :unprocessable_content
+    render file: Rails.root.join('public', '422.html'), layout: false, status: :unprocessable_entity
   end
 
   def internal_server_error
-    render status: :internal_server_error
+    render file: Rails.root.join('public', '500.html'), layout: false, status: :internal_server_error
   end
 end
