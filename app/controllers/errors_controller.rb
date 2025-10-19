caf808a6a3307f1d10bd303ab18cc6ad6d552621
Lodewiges <jamestreeg@gmail.com>
2025-10-19 23:36:38 +0200

    The last update made it static this looks better

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,18 +1,19 @@
 class ErrorsController < ApplicationController
+  layout "application"
 
   def forbidden
-    render file: Rails.root.join('public', '403.html'), layout: false, status: :forbidden
+    render template: 'errors/forbidden', status: :forbidden
   end
 
   def not_found
-    render file: Rails.root.join('public', '404.html'), layout: false, status: :not_found
+    render template: 'errors/not_found', status: :not_found
   end
 
   def unacceptable
-    render file: Rails.root.join('public', '422.html'), layout: false, status: :unprocessable_entity
+    render template: 'errors/unacceptable', status: :unprocessable_entity
   end
 
   def internal_server_error
-    render file: Rails.root.join('public', '500.html'), layout: false, status: :internal_server_error
+    render template: 'errors/internal_server_error', status: :internal_server_error
   end
 end
