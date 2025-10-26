39d68d90c2669b219d61f9d696fdd8a85ba8576d
Lodewiges <jamestreeg@gmail.com>
2025-10-26 19:29:41 +0100

    Puma was wrongly configured

--- config/puma.rb
+++ config/puma.rb
@@ -24,7 +24,7 @@ threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch('PORT', 3000)
+port ENV.fetch('PORT', 5000)
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
