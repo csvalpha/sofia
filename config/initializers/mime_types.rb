2e97565a22f5c56eb1bf32395dc01219b7e60c0b
Lodewiges <jamestreeg@gmail.com>
2025-11-22 22:53:29 +0100

    Intial commit message

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,6 +2,3 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
-Mime::Type.register 'font/woff', :woff
-Mime::Type.register 'font/woff2', :woff2
-Mime::Type.register 'font/ttf', :ttf
