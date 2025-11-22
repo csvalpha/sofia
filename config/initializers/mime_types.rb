5c509ccae815f57136a21e8f2d4e95c325116fdf
Lodewiges <jamestreeg@gmail.com>
2025-11-22 20:19:49 +0100

    add restriction mime types

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,6 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+Mime::Type.register "font/woff",  :woff
+Mime::Type.register "font/woff2", :woff2
+Mime::Type.register "font/ttf",   :ttf
