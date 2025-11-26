02bcefbbfb965e7ca900b0e19aa81269c4f8d556
Lodewiges <jamestreeg@gmail.com>
2025-11-26 14:52:01 +0100

    poging 2 to fix payment add

--- app/javascript/payment_add.js
+++ app/javascript/payment_add.js
@@ -1,29 +1,29 @@
 import Vue from 'vue/dist/vue.esm';
 import axios from 'axios';
 
-const MIN_PAYMENT_AMOUNT = 21;
+const MIN_PAYMENT_AMOUNT = 21.8;
 
 document.addEventListener('turbo:load', () => {
   axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-  
+
   const element = document.getElementById('payment-add');
   if (element !== null) {
     new Vue({
       el: element,
       data: () => {
         return {
-          currentCredit: parseFloat(element.dataset.userCredit) || 0,
+          currentCredit: parseFloat(element.dataset.userCredit),
           paymentAmount: parseFloat(element.dataset.paymentAmount) || MIN_PAYMENT_AMOUNT
         };
       },
       computed: {
         newCredit() {
-          return (this.currentCredit + this.paymentAmount).toFixed(2);
+          return (this.currentCredit + parseFloat(this.paymentAmount)).toFixed(2);
         }
       },
       methods: {
         amountValid() {
-          return this.paymentAmount >= MIN_PAYMENT_AMOUNT;
+          return parseFloat(this.paymentAmount) >= MIN_PAYMENT_AMOUNT;
         }
       },
     });
