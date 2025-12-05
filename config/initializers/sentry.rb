a8ffaa5a783f6ce32343e527cfb9c012471d8213
lodewiges <131907615+lodewiges@users.noreply.github.com>
2025-12-05 19:04:08 +0100

    Make a deploy target for Euros (#1145)
    * initial commit
    
    * fix PDF no being able to be emaild
    
    * add a pdf css file
    
    * fix lint

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 Sentry.init do |config|
   config.dsn = Rails.application.config.x.sentry_dsn
-  config.enabled_environments = %w[production staging luxproduction]
+  config.enabled_environments = %w[production staging luxproduction euros]
   config.environment = Rails.env
   config.release = ENV.fetch('BUILD_HASH', nil)
 end
