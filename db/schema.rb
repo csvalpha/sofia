5a588286658981557b1a8d2afe03b1da2a9fcbe4
lodewiges <131907615+lodewiges@users.noreply.github.com>
2025-12-09 17:54:17 +0100

    Add subprovider field to users & make seperate ENV variable for user visible IBAN (#1160)
    * Add subprovider field to users
    
    * fix lint
    
    * make the option for IBAN Configarable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_13_094147) do
+ActiveRecord::Schema[7.2].define(version: 2025_12_09_160709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -182,6 +182,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_13_094147) do
     t.boolean "deactivated", default: false, null: false
     t.string "activation_token"
     t.datetime "activation_token_valid_till"
+    t.string "sub_provider"
+    t.index ["sub_provider"], name: "index_users_on_sub_provider"
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
