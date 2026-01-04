deab376e5fc666326905d4956e84d1ee30c0f06c
Lodewiges <jamestreeg@gmail.com>
2026-01-05 00:20:57 +0100

    i could not simplfy it 2 env

---
+++ config/environments/luxproduction.rb
@@ -0,0 +1,6 @@
+require File.expand_path('production.rb', __dir__)
+
+Rails.application.configure do
+  # Only override production when applicable
+  # Keep in mind that luxproduction should reflect production as close as possible
+end
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,9 @@
+# Load enabled environments from deploy_targets.yml
+deploy_targets = YAML.load_file(Rails.root.join('config', 'deploy_targets.yml'))['targets']
+
 Sentry.init do |config|
   config.dsn = Rails.application.config.x.sentry_dsn
-  config.enabled_environments = %w[production staging]
+  config.enabled_environments = deploy_targets.keys.map(&:to_sym)
   config.environment = Rails.env
   config.release = ENV.fetch('BUILD_HASH', nil)
 end
