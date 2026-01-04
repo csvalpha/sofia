7f8d8689b37afcb8bf88e0e2869d455dd2b2224c
Lodewiges <jamestreeg@gmail.com>
2026-01-04 23:34:51 +0100

    made sentry.rb more readable

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,10 +1,9 @@
+# Load enabled environments from deploy_targets.yml
+deploy_targets = YAML.load_file(Rails.root.join('config', 'deploy_targets.yml'))['targets']
+
 Sentry.init do |config|
   config.dsn = Rails.application.config.x.sentry_dsn
-  
-  # Load enabled environments from deploy_targets.yml
-  deploy_targets = YAML.load_file(Rails.root.join('config', 'deploy_targets.yml'))['targets']
   config.enabled_environments = deploy_targets.keys
-  
   config.environment = Rails.env
   config.release = ENV.fetch('BUILD_HASH', nil)
 end
