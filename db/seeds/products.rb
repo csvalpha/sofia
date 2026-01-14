a2f7af6861ba6d3c1fdc932ec120c77254ed3716
lodewiges <131907615+lodewiges@users.noreply.github.com>
2026-01-14 01:36:05 +0100

    Improve the Seeding of sofia accounts (#1201)
    * generate better seeding files
    
    * intial commit message
    
    * fix suggestions
    
    * fix lint
    
    * fix lint

--- db/seeds/products.rb
+++ db/seeds/products.rb
@@ -1,4 +1,17 @@
 def seed_products # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+  # Category colors for products
+  category_colors = {
+    beer: '#FFD700', # Goud/geel - bierkleur
+    low_alcohol_beer: '#90EE90', # Lichtgroen - light/gezonder
+    craft_beer: '#CD7F32',     # Brons - premium/craft
+    non_alcoholic: '#87CEEB',  # Lichtblauw - fris/water
+    distilled: '#DC143C',      # Donkerrood - sterke drank
+    whiskey: '#8B4513',        # Bruin - whiskykleur
+    wine: '#722F37',           # Bordeaux - wijnkleur
+    food: '#FFA500',           # Oranje - eten
+    tobacco: '#808080',        # Grijs - rook
+    donation: '#9370DB'        # Paars - speciaal/donatie
+  }.freeze
   # rubocop:disable Style/WordArray
   products_beer = ['Bier (glas)', 'Bier (pul)', 'Bier (pitcher)', '12+1']
   products_low_alcohol_beer = ['Alcoholarm bier', 'Radler 0.0']
@@ -13,43 +26,43 @@ def seed_products # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metri
   # rubocop:enable Style/WordArray
 
   products = products_beer.map do |name|
-    Product.create(name:, category: :beer)
+    Product.create(name:, category: :beer, color: category_colors[:beer])
   end
 
   products_low_alcohol_beer.each do |name|
-    products << Product.create(name:, category: :low_alcohol_beer)
+    products << Product.create(name:, category: :low_alcohol_beer, color: category_colors[:low_alcohol_beer])
   end
 
   products_craft_beer.each do |name|
-    products << Product.create(name:, category: :craft_beer)
+    products << Product.create(name:, category: :craft_beer, color: category_colors[:craft_beer])
   end
 
   products_non_alcoholic.each do |name|
-    products << Product.create(name:, category: :non_alcoholic)
+    products << Product.create(name:, category: :non_alcoholic, color: category_colors[:non_alcoholic])
   end
 
   products_distilled.each do |name|
-    products << Product.create(name:, category: :distilled)
+    products << Product.create(name:, category: :distilled, color: category_colors[:distilled])
   end
 
   products_whiskey.each do |name|
-    products << Product.create(name:, category: :whiskey)
+    products << Product.create(name:, category: :whiskey, color: category_colors[:whiskey])
   end
 
   products_wine.each do |name|
-    products << Product.create(name:, category: :wine)
+    products << Product.create(name:, category: :wine, color: category_colors[:wine])
   end
 
   products_food.each do |name|
-    products << Product.create(name:, category: :food)
+    products << Product.create(name:, category: :food, color: category_colors[:food])
   end
 
   products_tobacco.each do |name|
-    products << Product.create(name:, category: :tobacco)
+    products << Product.create(name:, category: :tobacco, color: category_colors[:tobacco])
   end
 
   products_donation.each do |name|
-    products << Product.create(name:, category: :donation)
+    products << Product.create(name:, category: :donation, color: category_colors[:donation])
   end
 
   products
