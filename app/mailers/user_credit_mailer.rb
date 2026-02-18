9dd73102ab1380c2d312ec9f6cca3983c0ba5913
Lodewiges <jamestreeg@gmail.com>
2026-02-18 11:13:21 +0100

    revert my smart idea

--- app/mailers/user_credit_mailer.rb
+++ app/mailers/user_credit_mailer.rb
@@ -7,7 +7,11 @@ class UserCreditMailer < ApplicationMailer
     mail to: user.email, subject: 'Verzoek met betrekking tot uw Zatladder saldo'
   end
 
+  # rubocop:disable Metrics/AbcSize
   def credit_delivery_report_mail(success_count, unnotifyable_users)
+    @user = Struct.new(:name).new(
+      Rails.application.config.x.treasurer_name
+    )
     @unnotifyable_users = unnotifyable_users
     @success_count = success_count
     @title = 'Notificatie over de saldomail'
@@ -16,6 +20,7 @@ class UserCreditMailer < ApplicationMailer
 
     mail to: Rails.application.config.x.treasurer_email, subject:
   end
+  # rubocop:enable Metrics/AbcSize
 
   def new_credit_mutation_mail(credit_mutation)
     @user = credit_mutation.user
