7f1657429f686fa86aebd51730c441b6f19fa68a
Lodewiges <jamestreeg@gmail.com>
2026-09-03 13:32:49 +0200

    rename product price

--- app/controllers/product_price_folders_controller.rb
+++ app/controllers/product_price_folders_controller.rb
@@ -36,8 +36,8 @@ class ProductPriceFoldersController < ApplicationController
     orphaned_products = @folder.product_prices
     max_position = @folder.price_list.product_prices.without_folder.maximum(:position) || -1
 
-    orphaned_products.each_with_index do |product_price, index|
-      product_price.update(product_price_folder_id: nil, position: max_position + index + 1)
+    orphaned_products.each_with_index do |product_prices, index|
+      product_prices.update(product_price_folder_id: nil, position: max_position + index + 1)
     end
 
     @folder.destroy
