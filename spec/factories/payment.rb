f93361a6db9988b085876bfcc291b71886167bbd
Lodewiges <jamestreeg@gmail.com>
2026-09-15 10:43:26 +0200

    fix test and lint

--- spec/factories/payment.rb
+++ spec/factories/payment.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :payment do
     mollie_id { Faker::Name.initials(number: 4) }
-    amount { rand(20..1000) }
+    amount { rand(21.8..1000) }
     status { 'open' }
     user
 
